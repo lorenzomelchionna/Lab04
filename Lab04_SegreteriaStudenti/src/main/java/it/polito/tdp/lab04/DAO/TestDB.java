3526a412341c512002be2de87ad1bca9ed66afcb
User <User@DESKTOP-1JG37L3>
2021-04-14 15:21:56 +0200

    Versione Finale

--- Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/DAO/TestDB.java
+++ Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/DAO/TestDB.java
@@ -1,17 +1,9 @@
 package it.polito.tdp.lab04.DAO;
 
-import it.polito.tdp.lab04.model.Corso;
-import it.polito.tdp.lab04.model.Studente;
-
 public class TestDB {
 
 	public static void main(String[] args) {
-
-		CorsoDAO cdao = new CorsoDAO();
 	
-		boolean i = cdao.inscriviStudenteACorso(new Studente(146101, "VIGGIANO", "CLAUDIO"), new Corso("01NBAPG", 8, "Ingegneria della qualità",1));
-		
-		System.out.print(i);
 		
 	}
 
